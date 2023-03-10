58d15834fd7c2b2bff0288732f6965e4385ae6aa
Ajay P <repoapps@gmail.com>
2023-03-10 15:56:47 +0530

    2nd commit

--- app/src/main/java/com/repoapps/firstapp01/MainActivity.kt
+++ app/src/main/java/com/repoapps/firstapp01/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setTitle(R.string.app_title_01)
     }
 }
