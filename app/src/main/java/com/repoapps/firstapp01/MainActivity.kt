e1b1555bb90029f9f24d586250f3db509519b9a4
Ajay P <repoapps@gmail.com>
2023-03-10 17:03:32 +0530

    3rd commit

--- app/src/main/java/com/repoapps/firstapp01/MainActivity.kt
+++ app/src/main/java/com/repoapps/firstapp01/MainActivity.kt
@@ -2,11 +2,21 @@ package com.repoapps.firstapp01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.*
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setTitle(R.string.app_title_01)
+
+        findViewById<Button>(R.id.btnBaam)
+            .setOnClickListener {
+                Toast.makeText(applicationContext,R.string.app_button_log,Toast.LENGTH_SHORT).show()
+            }
+
     }
 }
